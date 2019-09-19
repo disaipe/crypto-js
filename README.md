8e968e66c9222507a433f605251f32dc697169a2
Evgeniy <disaipe@gmail.com>
2019-09-19 16:09:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,123 @@
+ 
+# Crypto-JS
+
+Библиотека для просмотра установленных сертификатов, подписания данных и верификации подписей с помощью [КриптоПро ЭЦП Browser plug-in](https://www.cryptopro.ru/products/cades/plugin).
+
+## Перед использованием
+
+Перед использованием Crypto-JS необходимо произвести установку *КриптоПро CSP* и *КриптоПро ЭЦП Browser plug-in* по инструкции ниже и запросить сертификат в удостоверяющем центре (например, в [тестовом центре КриптоПро](https://www.cryptopro.ru/certsrv/certrqma.asp)).
+
+**КриптоПро CSP**
+* Зарегистрироваться и войти на сайт https://www.cryptopro.ru/
+* Скачать сертифицированный  дистрибутив КриптоПро CSP на странице [загрузок](https://www.cryptopro.ru/products/csp/downloads)
+* Установить дистрибитив согласно официальной документации
+
+**КриптоПро ЭЦП Browser plug-in**
+* [MS Windows](https://cpdn.cryptopro.ru/default.asp?url=/content/cades/plugin-installation-windows.html)
+* [Linux](https://cpdn.cryptopro.ru/default.asp?url=/content/cades/plugin-installation-unix.html)
+* [Mac OS](https://cpdn.cryptopro.ru/default.asp?url=/content/cades/plugin-installation-macos.html)
+* [Полная инструкция](https://cpdn.cryptopro.ru/default.asp?url=content/cades/plugin.html)
+
+В результате установки *КриптоПро ЭЦП Browser plug-in* должен увидеть установленные в вашей системе сертификаты.
+
+Проверить работу плагина можно на специальной [странице](https://www.cryptopro.ru/sites/default/files/products/cades/demopage/simple.html)
+
+## Установка
+
+1. Скачать и сохранить [cryptohelper.js](cryptohelper.js)
+2. Подключить на странице вместе с `cadesplugin_api.js`:
+	```html
+	<script src='https://www.cryptopro.ru/sites/default/files/products/cades/cadesplugin_api.js'></script>
+	<script src='/cryptohelper.js'></script>
+	```
+
+## Использование
+
+Полный пример использования можно посмотреть на странице [examples/index.html](examples/index.html).
+
+#### Создание экземпляра
+```js
+let crypto = new CryptoHelper();
+crypto.init().then(() => {
+	// плагин инициализирован и готов к работе
+}).catch(() => {
+	// пользователь отклонил запрос
+});
+
+// здесь плагин еще не инизиализирован
+```
+
+При вызове `CryptoHelper.init()` у пользователя будет запрошено разрешение на выполнение операций с ключами или сертификатами - если пользователь отклонит запрос, то плагин не будет активирован и дальнейшая работа станет бесполезной.
+
+#### Получение сертификатов
+
+Их хранилища пользователя запрашиваются только активные и валидные сертификаты.
+
+```js
+let certificates = await crypto.getCertificates();
+// => [{
+// 		$original: Object,
+// 		subject: Object { name, email, company, city, ...},
+// 		issuer: Object { name, email, company, ...},
+// 		version: 3,
+// 		serialNumber: "12003B1267D658852004813D6F0001003B1267",
+// 		thumbprint: "75C9AD57EEC3A3E914FE1EDB518BD817EDE81797",
+// 		validFrom: "2019-09-18T11:38:09.000Z",
+// 		validTo: "2019-12-18T11:48:09.000Z",
+// 		hasPrivate: true,
+// 		isValid: true
+// }, ...]
+```
+
+#### Подписание
+
+Класс `Crypto` содержит несколько методов подписания данных:
+
+|Метод|Аргументы|Описание|
+|---|---|---|
+|sign|(Сertificate, String \| File \| FileList \| HtmlInputElement)|Общий метод|
+|signString|(Сertificate, String, toBase64 = true)|Подписание строки|
+|signFile|(Сertificate, File)|Подписание файла|
+|signFileList|(Сertificate, FileList)|Подписание списка файлов|
+
+Предпочтительней использовать общий метод, который автоматически выбирает нужны метод на основании переданных данных.
+
+```js
+// Подписание строки
+let secret = 'My secret string';
+let sign = await crypto.sign(certificate, secret);
+// => 'MIIIgAYJKoZIhvc...'
+```
+```js
+// Подписание файлов из <input id='filesToSign' type='file'>
+let filesInput = document.getElementById('filesToSign');
+let signs = await crypto.sign(certificate, filesInput);
+// => ['MIII4QYJKoZIhv...', 'MIII4QYJKoZIhvc...', ...]
+
+// Или так
+let signs = await crypto.sign(certificate, filesInput.files);
+
+// Подписание произвольного файла
+let sign = await crypto.sign(certificate, filesInput.files[0]);
+// => 'MIII4QYJKoZIhv...'
+```
+
+#### Верификация
+
+Для проверки подлинности подписи для документа используется метод `CryptoHelper.verify()`
+
+```js
+let data = 'My secret string';
+let sign = 'MIIIgAYJKoZIhvc...';
+
+let signInfo = await crypto.verify(data, sign);
+
+if (!signInfo) {
+	// Подпись не валидна
+} else {
+	// Подпись валидна, вывести список подписей
+	for (let sign in signInfo) {
+		console.log(`Timestamp: ${sign.ts}, Name: ${sign.cert.subject.name}`);
+	}
+}
+```
---
+++ cryptohelper.js
@@ -0,0 +1,355 @@
+window.CryptoHelper = function() {	
+	const self = this;
+	const crypto = cadesplugin;
+
+	const SIGN_DETACHED = true;
+	const VERBOSE = true;
+
+	self.isReady = false;
+
+	/**
+	 * Handle plugin init event
+	 * @returns {Promise}
+	 */
+	this.init = () => {
+		return new Promise((resolve, reject) => { 
+			crypto.then(async () => {
+				if (await check()) {
+					resolve();
+				} else {
+					reject(new Error('Cadesplugin not activated'));
+				}
+			});
+		});
+	};
+
+	/**
+	 * Get valid certificates from store
+	 * @returns {Array<Certificate>}
+	 */
+	this.getCertificates = () => {
+		return new Promise(async (resolve) => {
+			const store = await createStore();
+			store.Open(crypto.CAPICOM_CURRENT_USER_STORE, crypto.CAPICOM_MY_STORE, crypto.CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED);
+
+			let certs = await store.Certificates;
+			certs = await certs.Find(crypto.CAPICOM_CERTIFICATE_FIND_TIME_VALID);
+
+			const certificates = [];
+			for (let i = 1; i <= await certs.Count; i += 1) {
+				const cert = await certs.Item(i);
+				certificates.push(await parseCertificate(cert));
+			}
+
+			store.Close();
+
+			resolve(certificates);
+		});
+	};
+
+	/**
+	 * Sign various data type by chosen certificate
+	 * @param {Certificate} certificate
+	 * @param {String|File|DomElement} data
+	 * @returns {String}
+	 */
+	this.sign = (certificate, data) => {
+		if (data instanceof File) {
+			log('[Crypto] Signing File');
+			return this.signFile(certificate, data);
+		} else if (data instanceof FileList) {
+			log('[Crypto] Signing FileList');
+			return this.signFileList(certificate, data);
+		} else if (data instanceof HTMLInputElement && data.type === 'file') {
+			log('[Crypto] Signing file input');
+			return this.signFileList(certificate, data.files);
+		} else {
+			log('[Crypto] Signing string');
+			return this.signString(certificate, data);
+		}
+	};
+
+	/**
+	 * Sign string data by chosen certificate
+	 * @param {Certificate} certificate
+	 * @param {String} data
+	 * @param {Boolean} toBase64
+	 * @returns {String}
+	 */
+	this.signString = (certificate, data, toBase64 = true) => {
+		return new Promise(async (resolve, reject) => {
+			const signer = await createSigner();
+			const signedData = await createSignedData();
+
+			signer.propset_Certificate(certificate);
+			signer.propset_Options(crypto.CAPICOM_CERTIFICATE_INCLUDE_WHOLE_CHAIN)
+
+			signedData.propset_ContentEncoding(crypto.CADESCOM_BASE64_TO_BINARY);
+			signedData.propset_Content(toBase64 ? btoa(data) : data);
+
+			try {
+				const signedMessage = await signedData.SignCades(signer, crypto.CADESCOM_CADES_BES, SIGN_DETACHED);
+				resolve(signedMessage);
+			} catch (e) {
+				console.error('[Crypto] Sign failed', e);
+				reject(false);
+			}
+		});
+	};
+
+	/**
+	 * Sign file by chosen certificate
+	 * @param {Certificate} certificate
+	 * @param {File} file
+	 * @returns {String}
+	 */
+	this.signFile = (certificate, file) => {
+		return new Promise((resolve) => {
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = () => {
+				const header = ';base64,';
+				const fileData = reader.result;
+				const fileContent = fileData.substr(fileData.indexOf(header) + header.length);
+				resolve(this.signString(certificate, fileContent, false));
+			};
+		});
+	};
+
+	/**
+	 * Sign file list by chosen certificate
+	 * @param {Certificate} certificate
+	 * @param {FileList} fileList
+	 * @returns {Array<String>}
+	 */
+	this.signFileList = (certificate, fileList) => {
+		const promises = Array.from(fileList).map((file) => {
+			return this.signFile(certificate, file);
+		});
+
+		return Promise.all(promises);
+	};
+
+	/**
+	 * Verify data sign
+	 * @param {String} data
+	 * @param {String} sign
+	 * @param {Boolean} toBase64
+	 * @returns {Array<Object>}
+	 */
+	this.verify = (data, sign, toBase64 = false) => {
+		return new Promise(async (resolve) => {
+			const signedData = await createSignedData();
+
+			signedData.propset_ContentEncoding(crypto.CADESCOM_BASE64_TO_BINARY);
+			signedData.propset_Content(toBase64 ? btoa(data) : data);
+
+			try {
+				await signedData.VerifyCades(sign, crypto.CADESCOM_CADES_BES, true);
+				const signs = await signInfo(signedData);
+				resolve(signs);
+			} catch (e) {
+				console.error('[Crypto] Verify failed', e);
+				resolve(false);
+			}
+		});
+	};
+
+	/**
+	 * Get installed CSP version
+	 */
+	this.checkCSP = () => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let oAbout = await crypto.CreateObjectAsync('CAdESCOM.About');
+				let oVersion = await oAbout.CSPVersion();
+
+				const about = {
+					version: await oVersion.toString(),
+					name: await oAbout.CSPName()
+				};
+
+				resolve(about);
+			} catch (e) {
+				reject();
+			}
+		});
+	};
+
+	/**
+	 * Check cadesplugin is ready
+	 * (try create Store object)
+	 */
+	async function check() {
+		try {
+			await createStore();
+			return self.isReady = true;
+		} catch (e) {
+			return self.isReady = false;
+		}
+	}
+
+	/**
+	 * Create new CAdESCOM.Store instance
+	 * View more at https://cpdn.cryptopro.ru/content/cades/class_store.html
+	 * @returns {CAdESCOM.Store}
+	 */
+	async function createStore() {
+		return await crypto.CreateObjectAsync('CAdESCOM.Store');
+	}
+
+	/**
+	 * Create new CAdESCOM.CPSigner instance
+	 * View more at https://cpdn.cryptopro.ru/content/cades/class_c_ad_e_s_c_o_m_1_1_c_p_signer.html
+	 * @returns {CAdESCOM.CPSigner}
+	 */
+	async function createSigner() {
+		return await crypto.CreateObjectAsync('CAdESCOM.CPSigner');
+	}
+
+	/**
+	 * Create new CAdESCOM.CadesSignedData instance
+	 * View more at https://cpdn.cryptopro.ru/content/cades/class_c_ad_e_s_c_o_m_1_1_cades_signed_data.html
+	 * @returns {CAdESCOM.CadesSignedData}
+	 */
+	async function createSignedData() {
+		return await crypto.CreateObjectAsync('CAdESCOM.CadesSignedData');
+	}
+
+	/**
+	 * Extract certificate subject info
+	 * @param {Certificate} certificate 
+	 * @returns {Object}
+	 */
+	async function extractSubjectName(certificate) {
+		var subject = await certificate.SubjectName;
+		return parseDN(subject);
+	}
+
+	/**
+	 * Extract certificate issuer info
+	 * @param {Certificate} certificate 
+	 * @return {Object}
+	 */
+	async function extractIssuerName(certificate) {
+		var issuer = await certificate.IssuerName;			
+		return parseDN(issuer);
+	}
+
+	/**
+	 * Extract certificate info
+	 * @param {Certificate} certificate
+	 * @returns {Object}
+	 */
+	async function parseCertificate(certificate) {
+		const isValid = await certificate.IsValid();
+
+		return {
+			$original: certificate,
+			subject: await extractSubjectName(certificate),
+			issuer: await extractIssuerName(certificate),
+			version: await certificate.Version,
+			serialNumber: await certificate.SerialNumber,
+			thumbprint: await certificate.Thumbprint,
+			validFrom: await certificate.ValidFromDate,
+			validTo: await certificate.ValidToDate,
+			hasPrivate: await certificate.HasPrivateKey(),
+			isValid: await isValid.Result
+		}
+	}
+
+	/**
+	 * Extract signers from SignedData object
+	 * @param {CAdESCOM.CadesSignedData} signedData
+	 * @returns {Array<Object>}
+	 */
+	async function signInfo(signedData) {
+		const signers = await signedData.Signers;
+		const count = await signers.Count;
+
+		const signs = [];
+
+		for (let i = 1; i <= count; i += 1) {
+			const signer = await signers.Item(i);
+			const certificate = await signer.Certificate;
+
+			const sign = {
+				ts: await signer.SigningTime,
+				cert: await parseCertificate(certificate)
+			};
+
+			signs.push(sign);
+		}
+
+		return signs;
+	}
+
+	/**
+	 * Parse DN string to object
+	 * @param {String} dn
+	 * @returns {Object}
+	 */
+	function parseDN(dn) {
+		const tags = {
+			'CN': 'name',
+			'S': 'region',
+			'STREET': 'address',
+			'O': 'company',
+			'OU': 'postType',
+			'T': 'post',
+			'ОГРН': 'ogrn',
+			'СНИЛС': 'snils',
+			'ИНН': 'inn',
+			'E': 'email',
+			'L': 'city'
+		};
+
+		let buf = dn;
+		const fields = [...buf.matchAll(/(\w+)=/g)].reduceRight((acc, cur) => {
+			let v = buf.substring(cur.index);
+			v = v.replace(cur[0], '');
+			v = v.replace(/\s*"?(.*?)"?,?\s?$/, '$1');
+			v = v.replace(/""/g, '"');
+
+			const tag = cur[1];
+
+			if (tags[tag]) {
+				acc[tags[tag]] = v;
+			}
+
+			buf = buf.substring(0, cur.index);
+
+			return acc;
+		}, {});
+
+		return fields;
+	}
+
+	/**
+	 * Write verbose message to console
+	 * @param  {...any} args 
+	 */
+	function log(...args) {
+		if (VERBOSE) {
+			console.log(...args);
+		}
+	}
+}
+
+/**
+ * String.matchAll polyfill
+ */
+if (!String.prototype.matchAll) {
+	String.prototype.matchAll = function*(regex) {
+		function ensureFlag(flags, flag) {
+			return flags.includes(flag) ? flags : (flags + flag);
+		}
+
+		const localRegex = new RegExp(regex, ensureFlag(regex.flags, 'g'));
+
+		let match;
+		while (match = localRegex.exec(this)) {
+			yield match;
+		}
+	}
+}
